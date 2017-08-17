9f4b56f38e6471d8d9b2c2d30ebd701642d95858
Denis Raygorodskiy <raygorodskij@rapidapi.com>
2017-08-17 16:19:13 +0300

    update

--- src/routes/getAllCreativeFields.php
+++ src/routes/getAllCreativeFields.php
@@ -41,8 +41,7 @@ $app->post('/api/Behance/getAllCreativeFields', function ($request, $response) {
         }
     }
 
-    $url = $url.$queryParam['projectId'];
-    unset($queryParam['projectId']);
+
 
     $client = $this->httpClient;
 
